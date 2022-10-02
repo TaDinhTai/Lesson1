4261bc49338ea2bfc791b5c23253c3ee120e12f4
TaDinhTai <tadinhtai6868@gmail.com>
2022-10-02 10:41:18 +0700

    update list

--- lesson1/lib/testdart.dart
+++ lesson1/lib/testdart.dart
@@ -7,7 +7,7 @@ import 'dart:collection';
 import 'dart:collection';
 
 void main(){
-  printName("Hoang", "Nhi", middleName1: "Thi", middleName2: "Thanh");
+  printName("Hoang ", "Nhi", middleName1: "Thi", middleName2: "Thanh");
 }
 
 void printName(String firstName, String lastName, {required String middleName1,required String middleName2}){
